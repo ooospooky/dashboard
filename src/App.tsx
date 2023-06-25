b8da0b7c403a11e0615499b76b4d16f61449432c
Johnny Tai <bm414148@gmail.com>
2023-06-25 21:27:05 +0800

    brand done

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,20 @@ import './App.scss';
 function App() {
   return (
     <div className="App">
-      <div className='brand'>TAIWAN</div>
-      <div className='content'>CONTENT</div>
+      <div className="navbar"></div>
+      <div className='container'>
+        <div className='brand'>
+          <span className='brand__span'>TAIWAN</span>
+        </div>
+        <div className='content_container'>
+          <div className='content'>
+          <span >人口數、戶數按戶別及性別統計</span>
+          <form>
+            
+          </form>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/Components/Form.tsx
@@ -0,0 +1,9 @@
+import React,{FC} from 'react'
+
+export const Form:FC = () => {
+  return (
+    <div>
+      <select></select>
+    </div>
+  )
+}
