88f417c9c0f6f29b603eb2c13ab14ffd7a8fc76d
Johnny Tai <bm414148@gmail.com>
2023-06-26 01:13:21 +0800

    hr with text done

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.scss';
-import {Form} from './Components/Form'
+import { Form } from './Components/Form'
 function App() {
   return (
     <div className="App">
@@ -11,8 +11,13 @@ function App() {
         </div>
         <div className='content_container'>
           <div className='content'>
-          <span >人口數、戶數按戶別及性別統計</span>
+            <span >人口數、戶數按戶別及性別統計</span>
             <Form></Form>
+            <div className="hr-with-text">
+              <hr />
+              <span>搜尋結果</span>
+              <hr />
+            </div>
           </div>
         </div>
       </div>
