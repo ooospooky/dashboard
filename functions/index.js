d4951df96a3533126e3779c265faf5e856eae951
Johnny Tai <bm414148@gmail.com>
2023-06-27 16:01:57 +0800

    add comment

--- functions/index.js
+++ functions/index.js
@@ -11,12 +11,12 @@ const {onRequest} = require("firebase-functions/v2/https");
 const logger = require("firebase-functions/logger");
 const functions = require('firebase-functions');
 
-// 创建一个 HTTP Cloud Function
+// set HTTP Cloud Function
 exports.exampleFunction = functions.https.onRequest((request, response) => {
-  // 设置 CORS 头部
+  // set CORS head
   response.set('Access-Control-Allow-Origin', '*');
 
-  // 返回 JSON 响应
+  // return JSON 
   response.json({ message: 'Hello from Cloud Functions!' });
 });
 // Create and deploy your first functions
