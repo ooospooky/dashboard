e5545807643f80c1b8dbacdbeba965d345d80455
Johnny Tai <bm414148@gmail.com>
2023-06-27 15:39:17 +0800

    import Interface & clean console

--- src/Helper/FilterData.ts
+++ src/Helper/FilterData.ts
@@ -1,24 +1,9 @@
 import React from 'react'
+import Record from '../Interface/IRecord'
 
-interface Record {
-  statistic_yyy: string;
-  site_id: string;
-  village: string;
-  household_ordinary_total: string;
-  household_business_total: string;
-  household_single_total: string;
-  household_ordinary_m: string;
-  household_business_m: string;
-  household_single_m: string;
-  household_ordinary_f: string;
-  household_business_f: string;
-  household_single_f: string;
-}
 
 const FilterData = (allData: any, city: string, district: string) => {
-  console.log('FillterData', `${city}${district}`)
   const targetObject = allData.filter((obj: Record) => obj.site_id === `${city}${district}`)
-  console.log('targetObject', targetObject)
   let household_ordinary_f = 0;
   let household_ordinary_m = 0;
   let household_single_f = 0;
@@ -34,9 +19,7 @@ const FilterData = (allData: any, city: string, district: string) => {
     household_ordinary_total += Number(data["household_ordinary_total"])
     household_single_total += Number(data["household_single_total"])
   }
-  console.log('household_ordinary_m', household_ordinary_m)
-
-  return { 'household_ordinary_m': household_ordinary_m, 'household_ordinary_f': household_ordinary_f,
+  return { 'household_ordinary_m': household_ordinary_m,    'household_ordinary_f': household_ordinary_f,
   'household_single_f':household_single_f,
   'household_single_m':household_single_m,
   'household_ordinary_total':household_ordinary_total,
---
+++ src/Interface/IRecord.ts
@@ -0,0 +1,14 @@
+export default interface Record {
+  statistic_yyy: string;
+  site_id: string;
+  village: string;
+  household_ordinary_total: string;
+  household_business_total: string;
+  household_single_total: string;
+  household_ordinary_m: string;
+  household_business_m: string;
+  household_single_m: string;
+  household_ordinary_f: string;
+  household_business_f: string;
+  household_single_f: string;
+}
