c13f88df54e1f2b31cde3e0bc93388d41f43c64a
Johnny Tai <bm414148@gmail.com>
2023-07-07 13:58:53 +0800

    change targetObject to targetArray, cuz allData.filter will return array, so named targetObject is not good idea

--- src/Helper/FilterData.ts
+++ src/Helper/FilterData.ts
@@ -3,7 +3,7 @@ import Record from '../Interface/IRecord'
 
 
 const FilterData = (allData: any, city: string, district: string) => {
-  const targetObject = allData.filter((obj: Record) => obj.site_id === `${city}${district}`)
+  const targetArray = allData.filter((obj: Record) => obj.site_id === `${city}${district}`)
   let household_ordinary_f = 0;
   let household_ordinary_m = 0;
   let household_single_f = 0;
@@ -11,7 +11,7 @@ const FilterData = (allData: any, city: string, district: string) => {
   let household_ordinary_total = 0;
   let household_single_total = 0;
 
-  for (let data of targetObject) {
+  for (let data of targetArray) {
     household_ordinary_m += Number(data["household_ordinary_m"])
     household_ordinary_f += Number(data["household_ordinary_f"])
     household_single_f += Number(data["household_single_f"])
@@ -19,12 +19,13 @@ const FilterData = (allData: any, city: string, district: string) => {
     household_ordinary_total += Number(data["household_ordinary_total"])
     household_single_total += Number(data["household_single_total"])
   }
-  return { 'household_ordinary_m': household_ordinary_m,    'household_ordinary_f': household_ordinary_f,
-  'household_single_f':household_single_f,
-  'household_single_m':household_single_m,
-  'household_ordinary_total':household_ordinary_total,
-  'household_single_total':household_single_total
-}
+  return {
+    'household_ordinary_m': household_ordinary_m, 'household_ordinary_f': household_ordinary_f,
+    'household_single_f': household_single_f,
+    'household_single_m': household_single_m,
+    'household_ordinary_total': household_ordinary_total,
+    'household_single_total': household_single_total
+  }
 }
 
 export default FilterData;
